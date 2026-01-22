8859dc007d27591ef0fda6275dced289539e6f37
Vlad Kolesnikov <vladkol@google.com>
2026-01-22 12:53:00 -0800

    starter

--- evaluator/evaluate_agent.py
+++ evaluator/evaluate_agent.py
@@ -31,56 +31,5 @@ GOOGLE_CLOUD_PROJECT = os.environ["GOOGLE_CLOUD_PROJECT"]
 COMMIT_REVISION_TAG = os.getenv("COMMIT_REVISION_TAG", "latest")
 
 if __name__ == "__main__":
-    eval_data_researcher = os.path.dirname(__file__) + "/eval_data_researcher.json"
-    metrics=[
-        types.RubricMetric.FINAL_RESPONSE_MATCH,
-        types.RubricMetric.FINAL_RESPONSE_QUALITY,
-        types.RubricMetric.TOOL_USE_QUALITY,
-        trajectory_precision,
-        trajectory_recall
-    ]
-    eval_results = asyncio.run(evaluate_agent(
-        agent_api_server=RESEARCHER_URL,
-        agent_name="agent",
-        evaluation_data_file=eval_data_researcher,
-        evaluation_storage_uri=f"gs://{GOOGLE_CLOUD_PROJECT}-agents/evaluation",
-        metrics=metrics,
-        experiment_name="agent-evaluation-researcher",
-        run_name=f"{COMMIT_REVISION_TAG}--{int(time.time())}",
-    ))
-    researcher_eval_failed = False
-    print(f"\n🧪 Researcher Evaluation results:\n{json.dumps(eval_results, indent=2)}")
-    for metric_name, metric_values in eval_results.items():
-        if metric_values["mean"] < METRIC_THRESHOLD:
-            print(f"🛑 Researcher Evaluation failed with metric `{metric_name}` below {METRIC_THRESHOLD} threshold.")
-            researcher_eval_failed = True
-    if not researcher_eval_failed:
-        print(f"✅ Researcher Evaluation passed.")
-
-    eval_data_orchestrator = os.path.dirname(__file__) + "/eval_data_orchestrator.json"
-    metrics=[
-        # types.RubricMetric.FINAL_RESPONSE_QUALITY,
-        types.RubricMetric.HALLUCINATION,
-    ]
-    eval_results = asyncio.run(evaluate_agent(
-        agent_api_server=ORCHESTRATOR_URL,
-        agent_name="agent",
-        evaluation_data_file=eval_data_orchestrator,
-        evaluation_storage_uri=f"gs://{GOOGLE_CLOUD_PROJECT}-agents/evaluation",
-        metrics=metrics,
-        experiment_name="agent-evaluation-orchestrator",
-        run_name=f"{COMMIT_REVISION_TAG}--{int(time.time())}",
-    ))
-    orchestrator_eval_failed = False
-    print(f"\n🧪 Orchestrator Evaluation results:\n{json.dumps(eval_results, indent=2)}")
-    for metric_name, metric_values in eval_results.items():
-        if metric_values["mean"] < METRIC_THRESHOLD:
-            print(f"🛑 Orchestrator Evaluation failed with metric `{metric_name}` below {METRIC_THRESHOLD} threshold.")
-            orchestrator_eval_failed = True
-    if not orchestrator_eval_failed:
-        print(f"✅ Orchestrator Evaluation passed.")
-
-    if researcher_eval_failed or orchestrator_eval_failed:
-        exit(1)
-    else:
-        print(f"🎉 All Agent Evaluations passed.")
+    # TODO: implement evaluation
+    pass
